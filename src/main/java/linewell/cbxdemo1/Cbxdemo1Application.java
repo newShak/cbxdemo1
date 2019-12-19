30c67035addfe22cd0ca3c01ce3fa7fa3f85a7a4
newShak <569802195@qq.com>
2019-12-19 16:47:42 +0800

    Initial commit

---
+++ src/main/java/linewell/cbxdemo1/Cbxdemo1Application.java
@@ -0,0 +1,13 @@
+package linewell.cbxdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cbxdemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cbxdemo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/linewell/cbxdemo1/Cbxdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package linewell.cbxdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cbxdemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
